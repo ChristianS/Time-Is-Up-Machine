e5309bece6e9d78497640465b59164048367e52e
Christian Stropp <christian@stropp.org>
2014-05-03 21:56:13 +0200

    Three LEDs + alarm melody

--- time_is_up/time_is_up.ino
+++ time_is_up/time_is_up.ino
@@ -1,19 +1,27 @@
 // PINs
-const int redLedPin = 13;     // red LED pin
-const int greenLedPin = 9;     // green LED pin
+const int redLedPin = 9;     // red LED pin
+const int yellowLedPin = 7;  // yellow LED pin
+const int greenLedPin = 6;     // green LED pin
 const int buttonPin = 4;      // button input pin
 const int speakerPin = 3;    // speaker output pin
 
+// Button Debounce
 int buttonState = 0;
+int lastButtonState = LOW;   // the previous reading from the input pin
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;
 
+// Timers
 unsigned long startTime = 0;
+unsigned long alarmTimeInterval = 1000;
 
 boolean counting = false;
 
 void setup()
 {
   pinMode(redLedPin, OUTPUT);  
-  pinMode(greenLedPin, OUTPUT);      
+  pinMode(greenLedPin, OUTPUT);   
+  pinMode(yellowLedPin, OUTPUT);     
   pinMode(buttonPin, INPUT);
   pinMode(speakerPin, OUTPUT);
   
@@ -22,25 +30,67 @@ void setup()
 
 void loop()
 {
-  buttonState = digitalRead(buttonPin);
+  int reading = digitalRead(buttonPin);
   
-  if (buttonState == HIGH && counting == false)
-  {
-    counting = true;
-    startTime = millis();
-    digitalWrite(redLedPin, LOW);
-    digitalWrite(greenLedPin, HIGH);
-    Serial.println("button");
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  } 
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == HIGH) {
+        Serial.println("button");
+        
+        startTime = millis();
+        counting = true;
+        
+        digitalWrite(redLedPin, HIGH);
+        digitalWrite(yellowLedPin, HIGH);
+        digitalWrite(greenLedPin, LOW);
+      }
+    }
   }
   
+  lastButtonState = reading;
+  
   unsigned long currentTime = millis();
- 
-  if ((currentTime - startTime) > 5000 && counting == true)
+  
+  // half the time is up
+  if ((currentTime - startTime) > (alarmTimeInterval / 2) && counting == true)
   {
-    counting = false;
-    digitalWrite(redLedPin, HIGH); 
+    digitalWrite(redLedPin, LOW);
+    digitalWrite(yellowLedPin, HIGH);
     digitalWrite(greenLedPin, LOW);
-    tone(speakerPin, 300, 100);
-    Serial.println("beep");
+  }
+ 
+  // time is up
+  if ((currentTime - startTime) > alarmTimeInterval && counting == true)
+  {
+    Serial.println("alarm");
+    
+    digitalWrite(redLedPin, LOW); 
+    digitalWrite(yellowLedPin, LOW);
+    digitalWrite(greenLedPin, HIGH);
+    
+    // play melody
+    for (int i = 330; i < 523; i++)
+    {
+      tone(speakerPin, i, 7);
+      delay(7);
+    }
+    for (int i = 523; i > 440; i--)
+    {
+      tone(speakerPin, i, 10);
+      delay(10);
+    }
+    
+    counting = false;
   }
 }
