a76261114e7a5a298634df9ca68f5772a81f50fd
Christian Stropp <christian@stropp.org>
2014-05-04 17:06:30 +0200

    Five yellow lights + timer state management

--- time_is_up/time_is_up.ino
+++ time_is_up/time_is_up.ino
@@ -1,9 +1,13 @@
 // PINs
-const int redLedPin = 9;     // red LED pin
-const int yellowLedPin = 7;  // yellow LED pin
-const int greenLedPin = 6;     // green LED pin
-const int buttonPin = 4;      // button input pin
-const int speakerPin = 3;    // speaker output pin
+const int y1LedPin = 12;
+const int y2LedPin = 11;
+const int y3LedPin = 10;
+const int y4LedPin = 9;
+const int y5LedPin = 8;
+const int greenLedPin = 13;     // green LED pin
+const int buttonPin = 6;      // button input pin
+const int buttonPowerPin = 7;
+const int speakerPin = 5;    // speaker output pin
 
 // Button Debounce
 int buttonState = 0;
@@ -13,19 +17,104 @@ long debounceDelay = 50;
 
 // Timers
 unsigned long startTime = 0;
-unsigned long alarmTimeInterval = 1000;
+unsigned long alarmTimeInterval = 2000;
 
-boolean counting = false;
+// Timer state
+#define TS_Set5 5
+#define TS_Set4 4
+#define TS_Set3 3
+#define TS_Set2 2
+#define TS_Set1 1
+#define TS_Rest 0
+
+int timerState;
 
 void setup()
 {
-  pinMode(redLedPin, OUTPUT);  
-  pinMode(greenLedPin, OUTPUT);   
-  pinMode(yellowLedPin, OUTPUT);     
+  pinMode(y1LedPin, OUTPUT);  
+  pinMode(y2LedPin, OUTPUT);  
+  pinMode(y3LedPin, OUTPUT);  
+  pinMode(y4LedPin, OUTPUT);  
+  pinMode(y5LedPin, OUTPUT);  
+  pinMode(greenLedPin, OUTPUT);      
   pinMode(buttonPin, INPUT);
+  pinMode(buttonPowerPin, OUTPUT);
   pinMode(speakerPin, OUTPUT);
   
+  timerState = TS_Rest;
+  digitalWrite(buttonPowerPin, HIGH);
+  
   Serial.begin(9600);
+}
+
+// set all LEDs according to timer state
+void refreshLEDs()
+{
+  switch (timerState)
+  {
+    case TS_Set5:
+      digitalWrite(y1LedPin, HIGH);
+      digitalWrite(y2LedPin, HIGH);
+      digitalWrite(y3LedPin, HIGH);
+      digitalWrite(y4LedPin, HIGH);
+      digitalWrite(y5LedPin, HIGH);
+      digitalWrite(greenLedPin, LOW);
+      break;
+    case TS_Set4:
+      digitalWrite(y1LedPin, HIGH);
+      digitalWrite(y2LedPin, HIGH);
+      digitalWrite(y3LedPin, HIGH);
+      digitalWrite(y4LedPin, HIGH);
+      digitalWrite(y5LedPin, LOW);
+      digitalWrite(greenLedPin, LOW);
+      break;
+    case TS_Set3:
+      digitalWrite(y1LedPin, HIGH);
+      digitalWrite(y2LedPin, HIGH);
+      digitalWrite(y3LedPin, HIGH);
+      digitalWrite(y4LedPin, LOW);
+      digitalWrite(y5LedPin, LOW);
+      digitalWrite(greenLedPin, LOW);
+      break;
+    case TS_Set2:
+      digitalWrite(y1LedPin, HIGH);
+      digitalWrite(y2LedPin, HIGH);
+      digitalWrite(y3LedPin, LOW);
+      digitalWrite(y4LedPin, LOW);
+      digitalWrite(y5LedPin, LOW);
+      digitalWrite(greenLedPin, LOW);
+      break;
+    case TS_Set1:
+      digitalWrite(y1LedPin, HIGH);
+      digitalWrite(y2LedPin, LOW);
+      digitalWrite(y3LedPin, LOW);
+      digitalWrite(y4LedPin, LOW);
+      digitalWrite(y5LedPin, LOW);
+      digitalWrite(greenLedPin, LOW);
+      break;
+    case TS_Rest:
+      digitalWrite(y1LedPin, LOW);
+      digitalWrite(y2LedPin, LOW);
+      digitalWrite(y3LedPin, LOW);
+      digitalWrite(y4LedPin, LOW);
+      digitalWrite(y5LedPin, LOW);
+      digitalWrite(greenLedPin, HIGH);
+      break;
+  }
+}
+
+void playAlarmSound()
+{
+  for (int i = 330; i < 523; i++)
+  {
+    tone(speakerPin, i, 7);
+    delay(7);
+  }
+  for (int i = 523; i > 440; i--)
+  {
+    tone(speakerPin, i, 10);
+    delay(10);
+  }
 }
 
 void loop()
@@ -45,52 +134,47 @@ void loop()
     if (reading != buttonState) {
       buttonState = reading;
 
-      if (buttonState == HIGH) {
+      if (buttonState == HIGH) { // On button press ...
         Serial.println("button");
         
+        timerState = (timerState % 5) + 1; //... add one time interval
         startTime = millis();
-        counting = true;
         
-        digitalWrite(redLedPin, HIGH);
-        digitalWrite(yellowLedPin, HIGH);
-        digitalWrite(greenLedPin, LOW);
+        refreshLEDs();
       }
     }
   }
   
   lastButtonState = reading;
   
-  unsigned long currentTime = millis();
-  
-  // half the time is up
-  if ((currentTime - startTime) > (alarmTimeInterval / 2) && counting == true)
+  if (timerState > TS_Rest)
   {
-    digitalWrite(redLedPin, LOW);
-    digitalWrite(yellowLedPin, HIGH);
-    digitalWrite(greenLedPin, LOW);
-  }
- 
-  // time is up
-  if ((currentTime - startTime) > alarmTimeInterval && counting == true)
-  {
-    Serial.println("alarm");
+    unsigned long currentTime = millis();
+    unsigned long timePassed = currentTime - startTime;
     
-    digitalWrite(redLedPin, LOW); 
-    digitalWrite(yellowLedPin, LOW);
-    digitalWrite(greenLedPin, HIGH);
+    int oldState = timerState;
     
-    // play melody
-    for (int i = 330; i < 523; i++)
+    // check if time interval is up
+    if (timePassed > alarmTimeInterval)
     {
-      tone(speakerPin, i, 7);
-      delay(7);
-    }
-    for (int i = 523; i > 440; i--)
-    {
-      tone(speakerPin, i, 10);
-      delay(10);
+      timerState--;
+      
+      // restart timer for another interval
+      startTime = millis();
     }
     
-    counting = false;
+    // see if state changed
+    if (timerState != oldState)
+    {
+      refreshLEDs();
+      
+      if (timerState == TS_Rest) // Alarm - State just changed to Rest!
+      {
+        Serial.println("alarm");
+        
+        // play melody
+        playAlarmSound();
+      }
+    }
   }
 }
